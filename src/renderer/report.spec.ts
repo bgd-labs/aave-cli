caffcd259fc9b64faa96ab550e1d9645a283e9dd
Lukas <lukasstrassel@googlemail.com>
2023-04-26 15:34:37 +0200

    fix: update order (#3)

--- src/renderer/report.spec.ts
+++ src/renderer/report.spec.ts
@@ -7,6 +7,7 @@ describe("report", () => {
     const from = readJson("/src/mocks/preTestEngineArbV3.json");
     const to = readJson("/src/mocks/postTestEngineArbV3.json");
     const content = await diffReports(from, to);
+    console.log(content);
     expect(content).toMatchSnapshot();
   });
 });
