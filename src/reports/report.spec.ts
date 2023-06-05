c350ee1eefb53a9611565fbc463dec1a2dd46215
Lukas <lukasstrassel@googlemail.com>
2023-06-05 16:53:42 +0200

    fix: correctly snapshot v2-rates (#21)
    fixes a bug where an undefined parameter would break the api response

--- src/reports/report.spec.ts
+++ src/reports/report.spec.ts
@@ -1,11 +1,11 @@
-import { describe, it, expect } from "vitest";
-import { diffReports } from "../diffReports";
-import { readJson } from "../utils/json";
+import { describe, it, expect } from 'vitest';
+import { diffReports } from './diff-reports';
+import { readJsonFile } from '../utils/json';
 
-describe("report", () => {
-  it("should generate a well formatted report", async () => {
-    const from = readJson("/src/mocks/preTestEngineArbV3.json");
-    const to = readJson("/src/mocks/postTestEngineArbV3.json");
+describe('report', () => {
+  it('should generate a well formatted report', async () => {
+    const from = readJsonFile('/src/mocks/preTestEngineArbV3.json');
+    const to = readJsonFile('/src/mocks/postTestEngineArbV3.json');
     const content = await diffReports(from, to);
     console.log(content);
     expect(content).toMatchSnapshot();
--- src/reports/strategy.spec.ts
+++ src/reports/strategy.spec.ts
@@ -1,39 +1,49 @@
-import { describe, it, expect } from "vitest";
-import { diff } from "../utils/diff";
-import { AaveV3Reserve } from "../types";
-import { renderReserve, renderReserveDiff } from "./reserve";
-import { renderStrategy, renderStrategyDiff } from "./strategy";
+import { describe, it, expect } from 'vitest';
+import { diff } from './diff';
+import { renderStrategy, renderStrategyDiff } from './strategy';
+import { fetchRateStrategyImage } from './fetch-IR-strategy';
 
 const STRATEGY_1 = {
-  address: "0xfab05a6aF585da2F96e21452F91E812452996BD3",
-  baseStableBorrowRate: "50000000000000000000000000",
-  baseVariableBorrowRate: "10000000000000000000000000",
-  maxExcessStableToTotalDebtRatio: "800000000000000000000000000",
-  maxExcessUsageRatio: "200000000000000000000000000",
-  optimalStableToTotalDebtRatio: "200000000000000000000000000",
-  optimalUsageRatio: "800000000000000000000000000",
-  stableRateSlope1: "5000000000000000000000000",
-  stableRateSlope2: "750000000000000000000000000",
-  variableRateSlope1: "40000000000000000000000000",
-  variableRateSlope2: "750000000000000000000000000",
+  address: '0xfab05a6aF585da2F96e21452F91E812452996BD3',
+  baseStableBorrowRate: '50000000000000000000000000',
+  baseVariableBorrowRate: '10000000000000000000000000',
+  maxExcessStableToTotalDebtRatio: '800000000000000000000000000',
+  maxExcessUsageRatio: '200000000000000000000000000',
+  optimalStableToTotalDebtRatio: '200000000000000000000000000',
+  optimalUsageRatio: '800000000000000000000000000',
+  stableRateSlope1: '5000000000000000000000000',
+  stableRateSlope2: '750000000000000000000000000',
+  variableRateSlope1: '40000000000000000000000000',
+  variableRateSlope2: '750000000000000000000000000',
 };
 
 const STRATEGY_2 = {
-  address: "0xf4a0039F2d4a2EaD5216AbB6Ae4C4C3AA2dB9b82",
-  baseStableBorrowRate: "50000000000000000000000000",
-  baseVariableBorrowRate: "0",
-  maxExcessStableToTotalDebtRatio: "800000000000000000000000000",
-  maxExcessUsageRatio: "100000000000000000000000000",
-  optimalStableToTotalDebtRatio: "200000000000000000000000000",
-  optimalUsageRatio: "900000000000000000000000000",
-  stableRateSlope1: "5000000000000000000000000",
-  stableRateSlope2: "600000000000000000000000000",
-  variableRateSlope1: "40000000000000000000000000",
-  variableRateSlope2: "600000000000000000000000000",
+  address: '0xf4a0039F2d4a2EaD5216AbB6Ae4C4C3AA2dB9b82',
+  baseStableBorrowRate: '50000000000000000000000000',
+  baseVariableBorrowRate: '0',
+  maxExcessStableToTotalDebtRatio: '800000000000000000000000000',
+  maxExcessUsageRatio: '100000000000000000000000000',
+  optimalStableToTotalDebtRatio: '200000000000000000000000000',
+  optimalUsageRatio: '900000000000000000000000000',
+  stableRateSlope1: '5000000000000000000000000',
+  stableRateSlope2: '600000000000000000000000000',
+  variableRateSlope1: '40000000000000000000000000',
+  variableRateSlope2: '600000000000000000000000000',
 };
 
-describe("strategy", () => {
-  it("should properly render new strategy", () => {
+const STRATEGY_V2 = {
+  address: '0xc76EF342898f1AE7E6C4632627Df683FAD8563DD',
+  baseVariableBorrowRate: '0',
+  maxExcessUsageRatio: '310000000000000000000000000',
+  optimalUsageRatio: '690000000000000000000000000',
+  stableRateSlope1: '690000000000000000000000000',
+  stableRateSlope2: '600000000000000000000000000',
+  variableRateSlope1: '420000000000000000000000000',
+  variableRateSlope2: '600000000000000000000000000',
+};
+
+describe('strategy', () => {
+  it('should properly render new strategy', () => {
     const out = renderStrategy(STRATEGY_1);
     expect(out).eq(`| optimalUsageRatio | 80 % |
 | maxExcessUsageRatio | 20 % |
@@ -48,14 +58,17 @@ describe("strategy", () => {
 `);
   });
 
-  it("should properly render strategy diff", () => {
+  it('should properly render strategy diff', () => {
     const result = diff(STRATEGY_1, STRATEGY_2);
-    expect(renderStrategyDiff(result as any))
-      .eq(`| optimalUsageRatio | 80 % | 90 % |
+    expect(renderStrategyDiff(result as any)).eq(`| optimalUsageRatio | 80 % | 90 % |
 | maxExcessUsageRatio | 20 % | 10 % |
 | baseVariableBorrowRate | 1 % | 0 % |
 | variableRateSlope2 | 75 % | 60 % |
 | stableRateSlope2 | 75 % | 60 % |
 `);
+  });
+
+  it('should correctly render v2 strategies', async () => {
+    await fetchRateStrategyImage(STRATEGY_V2);
   });
 });
