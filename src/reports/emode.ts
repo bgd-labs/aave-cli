ec2652eb33edab40d8d1c179917d37dca08d5148
Lukas <lukasstrassel@googlemail.com>
2023-07-12 09:57:32 +0200

    feat: render eModes

---
+++ src/reports/emode.ts
@@ -0,0 +1,71 @@
+import { AaveV3Emode } from './snapshot-types';
+import { formatUnits } from 'viem';
+
+export function renderEModeValue<T extends keyof AaveV3Emode>(key: T, emode: AaveV3Emode) {
+  if (!emode[key]) return '-';
+  if (['reserveFactor', 'liquidationProtocolFee', 'liquidationThreshold', 'ltv'].includes(key))
+    return `${formatUnits(BigInt(emode[key]), 2)} %`;
+  if (key === 'liquidationBonus') return emode[key] === 0 ? '0 %' : `${((emode[key] as number) - 10000) / 100} %`;
+  return emode[key];
+}
+
+const ORDER: (keyof AaveV3Emode)[] = [
+  'eModeCategory',
+  'label',
+  'ltv',
+  'liquidationThreshold',
+  'liquidationBonus',
+  'priceSource',
+];
+function sortEmodeKeys(a: keyof AaveV3Emode, b: keyof AaveV3Emode) {
+  const indexA = ORDER.indexOf(a);
+  const indexB = ORDER.indexOf(b);
+  if (indexA != -1 && indexB != -1) {
+    if (indexA > indexB) {
+      return 1;
+    }
+    if (indexB > indexA) {
+      return -1;
+    }
+  }
+  if (indexA != -1) return -1;
+  if (indexB != -1) return -1;
+  return a.localeCompare(b);
+}
+
+const OMIT_KEYS: (keyof AaveV3Emode)[] = ['eModeCategory'];
+
+export function renderEmode(strategy: AaveV3Emode) {
+  let content = '';
+  (Object.keys(strategy) as (keyof AaveV3Emode)[])
+    .filter((key) => !OMIT_KEYS.includes(key))
+    .sort(sortEmodeKeys)
+    .map((key) => {
+      content += `| eMode.${key} | ${renderEModeValue(key, strategy)} |\n`;
+    });
+  return content;
+}
+
+export type EmodeDiff<A extends AaveV3Emode = AaveV3Emode> = {
+  [key in keyof AaveV3Emode]: A[key] & {
+    from: A[key] | null;
+    to: A[key] | null;
+  };
+};
+
+export function renderEmodeDiff(diff: EmodeDiff) {
+  let content = '';
+
+  (Object.keys(diff) as (keyof AaveV3Emode)[])
+    .filter((key) => !OMIT_KEYS.includes(key))
+    .filter((key) => diff[key].hasOwnProperty('from'))
+    .sort(sortEmodeKeys)
+    .map((key) => {
+      content += `| eMode.${key} | ${renderEModeValue(key, {
+        ...diff,
+        [key]: diff[key].from,
+      })} | ${renderEModeValue(key, { ...diff, [key]: diff[key].to })} |\n`;
+    });
+
+  return content;
+}
--- src/reports/report.spec.ts
+++ src/reports/report.spec.ts
@@ -4,8 +4,8 @@ import { readJsonFile } from '../utils/json';
 
 describe('report', () => {
   it('should generate a well formatted report', async () => {
-    const from = readJsonFile('/src/mocks/preTestEngineArbV3.json');
-    const to = readJsonFile('/src/mocks/postTestEngineArbV3.json');
+    const from = readJsonFile('/src/reports/mocks/preTestEngineArbV3.json');
+    const to = readJsonFile('/src/reports/mocks/postTestEngineArbV3.json');
     const content = await diffReports(from, to);
     console.log(content);
     expect(content).toMatchSnapshot();
