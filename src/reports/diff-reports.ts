394780b91cd8926fca63b4a969b8451c04fc28c3
Lukas <lukasstrassel@googlemail.com>
2023-07-12 09:58:33 +0200

    feat: render eModes (#32)

--- src/reports/diff-reports.ts
+++ src/reports/diff-reports.ts
@@ -1,14 +1,12 @@
-import hash from "object-hash";
-import { fetchRateStrategyImage } from "./fetch-IR-strategy";
-import { renderReserve, renderReserveDiff } from "./reserve";
-import { renderStrategy, renderStrategyDiff } from "./strategy";
-import { AaveV3Snapshot, AaveV3Reserve } from "./snapshot-types";
-import { diff } from "./diff";
+import hash from 'object-hash';
+import { fetchRateStrategyImage } from './fetch-IR-strategy';
+import { renderReserve, renderReserveDiff } from './reserve';
+import { renderStrategy, renderStrategyDiff } from './strategy';
+import { AaveV3Snapshot, AaveV3Reserve } from './snapshot-types';
+import { diff } from './diff';
+import { renderEmode, renderEmodeDiff } from './emode';
 
-export async function diffReports<
-  A extends AaveV3Snapshot,
-  B extends AaveV3Snapshot
->(pre: A, post: B) {
+export async function diffReports<A extends AaveV3Snapshot, B extends AaveV3Snapshot>(pre: A, post: B) {
   const chainId = pre.chainId;
   const diffResult = diff(pre, post);
   // download interest rate strategies
@@ -21,29 +19,24 @@ export async function diffReports<
   }
 
   // create report
-  let content = "";
+  let content = '';
   const reservesAdded = Object.keys(diffResult.reserves)
     .map((reserveKey) => {
       // to being present on reserve level % trueish means reserve was added
       if ((diffResult.reserves[reserveKey] as any).to) {
-        let report = renderReserve(
-          (diffResult.reserves[reserveKey] as any).to,
-          chainId
-        );
+        let report = renderReserve((diffResult.reserves[reserveKey] as any).to, chainId);
         const imageHash = hash(
-          post.strategies[
-            ((diffResult.reserves[reserveKey] as any).to as AaveV3Reserve)
-              .interestRateStrategy
-          ]
+          post.strategies[((diffResult.reserves[reserveKey] as any).to as AaveV3Reserve).interestRateStrategy]
         );
         report += renderStrategy(
-          post.strategies[
-            ((diffResult.reserves[reserveKey] as any).to as AaveV3Reserve)
-              .interestRateStrategy
-          ]
+          post.strategies[((diffResult.reserves[reserveKey] as any).to as AaveV3Reserve).interestRateStrategy]
         );
 
-        report += `| interestRate | ![ir](/.assets/${imageHash}.svg) |`;
+        report += `| interestRate | ![ir](/.assets/${imageHash}.svg) |\n`;
+        if (post.reserves[reserveKey].eModeCategory != 0) {
+          console.log(post.reserves[reserveKey].eModeCategory, post.eModes);
+          report += renderEmode(post.eModes[post.reserves[reserveKey].eModeCategory]);
+        }
 
         return report;
       }
@@ -53,10 +46,7 @@ export async function diffReports<
     .map((reserveKey) => {
       // from being present on reserve level % trueish means reserve was removed
       if ((diffResult.reserves[reserveKey] as any).from) {
-        return renderReserve(
-          (diffResult.reserves[reserveKey] as any).from,
-          chainId
-        );
+        return renderReserve((diffResult.reserves[reserveKey] as any).from, chainId);
       }
     })
     .filter((i) => i);
@@ -64,82 +54,59 @@ export async function diffReports<
     .map((reserveKey) => {
       // from being present on key means reserve was removed
       if (
-        !(diffResult.reserves[reserveKey] as any).hasOwnProperty("from") &&
+        !(diffResult.reserves[reserveKey] as any).hasOwnProperty('from') &&
         Object.keys(diffResult.reserves[reserveKey]).find(
-          (fieldKey) =>
-            typeof diffResult.reserves[reserveKey][fieldKey] === "object"
+          (fieldKey) => typeof diffResult.reserves[reserveKey][fieldKey] === 'object'
         )
       ) {
-        let report = renderReserveDiff(
-          diffResult.reserves[reserveKey] as any,
-          chainId
-        );
-        if (
-          diffResult.reserves[reserveKey].interestRateStrategy.hasOwnProperty(
-            "from"
-          )
-        ) {
+        let report = renderReserveDiff(diffResult.reserves[reserveKey] as any, chainId);
+        if (diffResult.reserves[reserveKey].interestRateStrategy.hasOwnProperty('from')) {
           report += renderStrategyDiff(
             diff(
-              pre.strategies[
-                (diffResult.reserves[reserveKey].interestRateStrategy as any)
-                  .from
-              ],
-              post.strategies[
-                (diffResult.reserves[reserveKey].interestRateStrategy as any).to
-              ]
+              pre.strategies[(diffResult.reserves[reserveKey].interestRateStrategy as any).from],
+              post.strategies[(diffResult.reserves[reserveKey].interestRateStrategy as any).to]
             ) as any
           );
           report += `| interestRate | ![before](/.assets/${hash(
-            pre.strategies[
-              (diffResult.reserves[reserveKey].interestRateStrategy as any).from
-            ]
+            pre.strategies[(diffResult.reserves[reserveKey].interestRateStrategy as any).from]
           )}.svg) | ![after](/.assets/${hash(
-            post.strategies[
-              (diffResult.reserves[reserveKey].interestRateStrategy as any).to
-            ]
+            post.strategies[(diffResult.reserves[reserveKey].interestRateStrategy as any).to]
           )}.svg) |`;
+        }
+        if (diffResult.reserves[reserveKey].eModeCategory.hasOwnProperty('from')) {
+          report += renderEmodeDiff(
+            diff(
+              pre.eModes[(diffResult.reserves[reserveKey].eModeCategory as any).from] || {},
+              post.eModes[(diffResult.reserves[reserveKey].eModeCategory as any).to]
+            ) as any
+          );
         }
 
         return report;
       }
     })
     .filter((i) => i);
-  if (
-    reservesAdded.length ||
-    reservesRemoved.length ||
-    reservesAltered.length
-  ) {
-    content += "## Reserve changes\n\n";
+  if (reservesAdded.length || reservesRemoved.length || reservesAltered.length) {
+    content += '## Reserve changes\n\n';
     if (reservesAdded.length) {
-      content += `### ${
-        reservesAdded.length > 1 ? "Reserve" : "Reserves"
-      } added\n\n`;
-      content += reservesAdded.join("\n\n");
-      content += "\n\n";
+      content += `### ${reservesAdded.length > 1 ? 'Reserve' : 'Reserves'} added\n\n`;
+      content += reservesAdded.join('\n\n');
+      content += '\n\n';
     }
 
     if (reservesAltered.length) {
-      content += `### ${
-        reservesAltered.length > 1 ? "Reserve" : "Reserves"
-      } altered\n\n`;
-      content += reservesAltered.join("\n\n");
-      content += "\n\n";
+      content += `### ${reservesAltered.length > 1 ? 'Reserve' : 'Reserves'} altered\n\n`;
+      content += reservesAltered.join('\n\n');
+      content += '\n\n';
     }
 
     if (reservesRemoved.length) {
-      content += `### ${
-        reservesRemoved.length > 1 ? "Reserve" : "Reserves"
-      } removed\n\n`;
-      content += reservesRemoved.join("\n\n");
-      content += "\n\n";
+      content += `### ${reservesRemoved.length > 1 ? 'Reserve' : 'Reserves'} removed\n\n`;
+      content += reservesRemoved.join('\n\n');
+      content += '\n\n';
     }
   }
 
-  content += `## Raw diff\n\n\`\`\`json\n${JSON.stringify(
-    diff(pre, post, true),
-    null,
-    2
-  )}\n\`\`\``;
+  content += `## Raw diff\n\n\`\`\`json\n${JSON.stringify(diff(pre, post, true), null, 2)}\n\`\`\``;
   return content;
 }
---
+++ src/reports/emode.spec.ts
@@ -0,0 +1,43 @@
+import { describe, it, expect } from 'vitest';
+import { diff } from './diff';
+import { fetchRateStrategyImage } from './fetch-IR-strategy';
+import { renderEmode, renderEmodeDiff } from './emode';
+
+const EMODE_1 = {
+  eModeCategory: 1,
+  label: 'Stablecoins',
+  liquidationBonus: 10100,
+  liquidationThreshold: 9750,
+  ltv: 9700,
+  priceSource: '0x0000000000000000000000000000000000000000',
+};
+
+const EMODE_2 = {
+  eModeCategory: 2,
+  label: 'Stablecoins (altered)',
+  liquidationBonus: 1000,
+  liquidationThreshold: 500,
+  ltv: 250,
+  priceSource: '0x0000000000000000000000000000000000000000',
+};
+
+describe('eMode', () => {
+  it('should properly render new strategy', () => {
+    const out = renderEmode(EMODE_1);
+    expect(out).eq(`| eMode.label | Stablecoins |
+| eMode.ltv | 97 % |
+| eMode.liquidationThreshold | 97.5 % |
+| eMode.liquidationBonus | 1 % |
+| eMode.priceSource | 0x0000000000000000000000000000000000000000 |
+`);
+  });
+
+  it('should properly render strategy diff', () => {
+    const result = diff(EMODE_1, EMODE_2);
+    expect(renderEmodeDiff(result as any)).eq(`| eMode.label | Stablecoins | Stablecoins (altered) |
+| eMode.ltv | 97 % | 2.5 % |
+| eMode.liquidationThreshold | 97.5 % | 5 % |
+| eMode.liquidationBonus | 1 % | -90 % |
+`);
+  });
+});
