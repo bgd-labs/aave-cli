b9f43f747719e444e1e902ba003b089a42724fc2
Lukas <lukasstrassel@googlemail.com>
2023-09-11 10:45:38 +0200

    fix: test fix

--- src/simulate/govv3/utils/solidityUtils.spec.ts
+++ src/simulate/govv3/utils/solidityUtils.spec.ts
@@ -3,9 +3,9 @@ import { setBits } from './solidityUtils';
 
 describe('solidityUtils', () => {
   it('setBits', async () => {
-    expect(setBits('0b11', 1n, 2n, 0n)).toBe('1');
-    expect(setBits('0b111', 1n, 2n, 0n)).toBe('5');
-    expect(setBits('0b111', 1n, 3n, 0n)).toBe('1');
-    expect(setBits('0b111', 0n, 3n, 0n)).toBe('0');
+    expect(setBits('0b11', 1n, 2n, 0n)).toBe(1n);
+    expect(setBits('0b111', 1n, 2n, 0n)).toBe(5n);
+    expect(setBits('0b111', 1n, 3n, 0n)).toBe(1n);
+    expect(setBits('0b111', 0n, 3n, 0n)).toBe(0n);
   });
 });
