77da3e6a0240463f5e14ba9842d4f6a6dc94901d
efecarranza <fcarr004@fiu.edu>
2023-11-21 15:56:18 -0500

    feat: return all the parameters for arbitrum bridge

--- src/commands/arbitrumBridgeExit.ts
+++ src/commands/arbitrumBridgeExit.ts
@@ -1,7 +1,6 @@
 import { Command } from '@commander-js/extra-typings';
 import { providers, Wallet } from 'ethers';
-import { L2ToL1MessageStatus, L2TransactionReceipt } from '@arbitrum/sdk';
-// require('dotenv').config()
+import { L2ToL1Message, L2TransactionReceipt } from '@arbitrum/sdk';
 
 export function addCommand(program: Command) {
     program
@@ -9,13 +8,8 @@ export function addCommand(program: Command) {
         .description('generate necessary parameters to exit arbitrum bridge')
         .argument('<tx_hash>')
         .argument('<log_index>')
-        .action(async (tx_hash, log_index) => {
-            if (!tx_hash) {
-                throw new Error(
-                    'Provide a transaction hash of an L2 transaction that sends an L2 to L1 message'
-                );
-            }
-    
+        .argument('<block_number>')
+        .action(async (tx_hash, log_index, block_number) => {
             if (!tx_hash.startsWith('0x') || tx_hash.trim().length != 66) {
                 throw new Error(`${tx_hash} doesn't look like a txn hash.`);
             }
@@ -25,7 +19,7 @@ export function addCommand(program: Command) {
             const l1Provider = new providers.JsonRpcProvider(process.env.RPC_MAINNET);
             const l2Provider = new providers.JsonRpcProvider(process.env.RPC_ARBITRUM);
             const walletPrivateKey = process.env.PRIVATE_KEY ?? '';
-            const l1Wallet = new Wallet(walletPrivateKey, l1Provider)
+            const l1Wallet = new Wallet(walletPrivateKey, l1Provider);
 
             const receipt = await l2Provider.getTransactionReceipt(tx_hash);
             const l2Receipt = new L2TransactionReceipt(receipt);
@@ -33,13 +27,32 @@ export function addCommand(program: Command) {
             const messages = await l2Receipt.getL2ToL1Messages(l1Wallet);
             const l2ToL1Msg = messages[idx];
 
-            if ((await l2ToL1Msg.status(l2Provider)) == L2ToL1MessageStatus.EXECUTED) {
-                console.log(`Message already executed! Nothing else to do here`);
-                process.exit(1);
-            }
+            // if ((await l2ToL1Msg.status(l2Provider)) == L2ToL1MessageStatus.EXECUTED) {
+            //     console.log(`Message already executed! Nothing else to do here`);
+            //     process.exit(1);
+            // }
 
-            const res = await l2ToL1Msg.execute(l2Provider);
-            const rec = await res.wait();
-            console.log('Transaction executed', rec);
+            const proof = await l2ToL1Msg.getOutboxProof(l2Provider);
+            console.log("Proof:", proof);
+
+            const block = parseInt(block_number);
+
+            const logs = await L2ToL1Message.getL2ToL1Events(l2Provider, { fromBlock: block, toBlock: block + 1 });
+            logs.forEach(function (log) {
+                if (log.transactionHash != tx_hash) {
+                    return;
+                }
+
+                console.log("Tx Hash:", log.transactionHash);
+                console.log("Index:", log.position.toNumber());
+                console.log("Caller:", log.caller);
+                console.log("Destination:", log.destination);
+                console.log("Arbitrum Block Number:", log.arbBlockNum.toNumber());
+                console.log("Mainnet Block Number:", log.ethBlockNum.toNumber());
+                console.log("Timestamp:", log.timestamp.toNumber());
+                console.log("Value:", log.callvalue.toNumber());
+                console.log("Data:", log.data);
+            });
+            
         });
 }
--- src/commands/optimismBridgeExit.ts
+++ src/commands/optimismBridgeExit.ts
@@ -1,5 +1,5 @@
 import { Command } from '@commander-js/extra-typings';
-import { providers } from 'ethers';
+import { providers, Wallet } from 'ethers';
 import { CrossChainMessenger } from '@eth-optimism/sdk';
 // require('dotenv').config()
 
@@ -8,30 +8,36 @@ export function addCommand(program: Command) {
     const l1ChainId = 1;
     const l2ChainId = 10;
 
-    const crossChainMessenger = new CrossChainMessenger({
-        l1ChainId: l1ChainId,
-        l2ChainId: l2ChainId,
-        l1SignerOrProvider: l1Wallet,
-        l2SignerOrProvider: l2Wallet
-    });
+    // const l1Provider = new providers.JsonRpcProvider(process.env.RPC_MAINNET);
+    // const l2Provider = new providers.JsonRpcProvider(process.env.RPC_ARBITRUM);
+    // const walletPrivateKey = process.env.PRIVATE_KEY ?? '';
+    // const l1Wallet = new Wallet(walletPrivateKey, l1Provider);
+    // const l2Wallet = new Wallet(walletPrivateKey, l2Provider);
 
-    optimism
-        .command('prove-messsage')
-        .description('generate proof of l2 to l1 bridge on Optimism')
-        .argument('<tx_hash>')
-        .argument('<log_index>')
-        .action(async (tx_hash, log_index) => {
-            await crossChainMessenger.proveMessage(tx_hash);
-            console.log("Message was proven, bridged assets will be available in ~7 days");
-    });
+    // const crossChainMessenger = new CrossChainMessenger({
+    //     l1ChainId: l1ChainId,
+    //     l2ChainId: l2ChainId,
+    //     l1SignerOrProvider: l1Wallet,
+    //     l2SignerOrProvider: l2Wallet
+    // });
 
-    optimism
-        .command('exit-bridge')
-        .description('exit Optimism bridge')
-        .argument('<tx_hash>')
-        .argument('<log_index>')
-        .action(async (tx_hash, log_index) => {
-            await crossChainMessenger.finalizeMessage(tx_hash);
-            console.log("Bridge exit was successful");
-    });
+    // optimism
+    //     .command('prove-messsage')
+    //     .description('generate proof of l2 to l1 bridge on Optimism')
+    //     .argument('<tx_hash>')
+    //     .argument('<log_index>')
+    //     .action(async (tx_hash, log_index) => {
+    //         await crossChainMessenger.proveMessage(tx_hash);
+    //         console.log("Message was proven, bridged assets will be available in ~7 days");
+    // });
+
+    // optimism
+    //     .command('exit-bridge')
+    //     .description('exit Optimism bridge')
+    //     .argument('<tx_hash>')
+    //     .argument('<log_index>')
+    //     .action(async (tx_hash, log_index) => {
+    //         await crossChainMessenger.finalizeMessage(tx_hash);
+    //         console.log("Bridge exit was successful");
+    // });
 }
