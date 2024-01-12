e8da9d26c989ee5a602cac8dca1cd65003c7ac93
Lukas <lukasstrassel@googlemail.com>
2024-01-12 22:30:51 +0100

    fix: use ipfs cache (#89)

--- src/govv3/simulate.ts
+++ src/govv3/simulate.ts
@@ -4,6 +4,8 @@ import { getGovernance } from './governance';
 import { Hex, PublicClient } from 'viem';
 import { PayloadsController, getPayloadsController } from './payloadsController';
 import { CHAIN_ID_CLIENT_MAP } from '@bgd-labs/js-utils';
+import { generateReport } from './generatePayloadReport';
+import { generateProposalReport } from './generateProposalReport';
 
 /**
  * Reference implementation, unused
@@ -17,7 +19,16 @@ export async function simulateProposal(governanceAddress: Hex, publicClient: Pub
   const governance = getGovernance({ address: governanceAddress, publicClient });
   const logs = await governance.cacheLogs();
   const proposal = await governance.getProposalAndLogs(proposalId, logs);
-  await governance.simulateProposalExecutionOnTenderly(proposalId, proposal);
+  const result = await governance.simulateProposalExecutionOnTenderly(proposalId, proposal);
+  console.log(
+    await generateProposalReport({
+      simulation: result,
+      proposalId: proposalId,
+      proposalInfo: proposal,
+      publicClient: publicClient,
+    })
+  );
+  return;
   const payloads: {
     payload: Awaited<ReturnType<PayloadsController['getPayload']>>;
     simulation: TenderlySimulationResponse;
@@ -25,12 +36,20 @@ export async function simulateProposal(governanceAddress: Hex, publicClient: Pub
   for (const payload of proposal.proposal.payloads) {
     const controllerContract = getPayloadsController(
       payload.payloadsController,
-      CHAIN_ID_CLIENT_MAP[Number(payload.chain) as keyof typeof CHAIN_ID_CLIENT_MAP] as PublicClient
+      CHAIN_ID_CLIENT_MAP[Number(payload.chain) as keyof typeof CHAIN_ID_CLIENT_MAP]
     );
     const logs = await controllerContract.cacheLogs();
     const config = await controllerContract.getPayload(payload.payloadId, logs);
     try {
       const result = await controllerContract.simulatePayloadExecutionOnTenderly(payload.payloadId, config);
+      console.log(
+        await generateReport({
+          simulation: result,
+          payloadId: payload.payloadId,
+          payloadInfo: config,
+          publicClient: CHAIN_ID_CLIENT_MAP[Number(payload.chain) as keyof typeof CHAIN_ID_CLIENT_MAP],
+        })
+      );
       payloads.push({ payload: config, simulation: result });
     } catch (e) {
       console.log('error simulating payload');
