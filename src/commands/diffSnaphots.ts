cf8d91747260cb1970fce856147ef6836f4a81ae
Lukas <lukasstrassel@googlemail.com>
2024-01-25 12:43:16 +0100

    feat: add storage diff (#104)

--- src/commands/diffSnaphots.ts
+++ src/commands/diffSnaphots.ts
@@ -2,6 +2,7 @@ import { Command } from '@commander-js/extra-typings';
 import { diffReports } from '../reports/diff-reports';
 import { readJsonString, readJsonFile } from '../utils/json';
 import fs from 'fs';
+import { compareStorageLayouts } from '@bgd-labs/js-utils';
 
 export function addCommand(program: Command) {
   program
@@ -15,6 +16,23 @@ export function addCommand(program: Command) {
       const from = options.stringMode ? readJsonString(_from) : readJsonFile(_from);
       const to = options.stringMode ? readJsonString(_to) : readJsonFile(_to);
       const content = await diffReports(from, to);
+      if (options.out) {
+        fs.writeFileSync(options.out, content);
+      } else {
+        console.log(content);
+      }
+    });
+
+  program
+    .command('diff-storage')
+    .description('generate a storage diff')
+    .argument('<from>')
+    .argument('<to>')
+    .option('-o, --out <string>', 'output path')
+    .action(async (_from, _to, options) => {
+      const from = readJsonFile(_from);
+      const to = readJsonFile(_to);
+      const content = await compareStorageLayouts(from, to);
       if (options.out) {
         fs.writeFileSync(options.out, content);
       } else {
