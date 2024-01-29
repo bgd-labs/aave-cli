84f7bedbf59ec39268e91b597b410339b298631f
Lukas <lukasstrassel@googlemail.com>
2024-01-29 09:46:20 +0100

    feat: allow simulation of local payload (#73)
    * fix: support local payloads
    
    * fix: patch some related issues

--- src/commands/fork.ts
+++ src/commands/fork.ts
@@ -2,9 +2,13 @@ import { Command } from '@commander-js/extra-typings';
 import { tenderly } from '../utils/tenderlyClient';
 import { getGovernance } from '../govv3/governance';
 import { getPayloadsController } from '../govv3/payloadsController';
-import { Hex, PublicClient } from 'viem';
-import { DEFAULT_GOVERNANCE, DEFAULT_GOVERNANCE_CLIENT } from '../utils/constants';
+import { Hex, createWalletClient, getContract, http } from 'viem';
+import { DEFAULT_GOVERNANCE, DEFAULT_GOVERNANCE_CLIENT, EOA } from '../utils/constants';
 import { CHAIN_ID_CLIENT_MAP } from '@bgd-labs/js-utils';
+import { findPayloadsController } from '../govv3/utils/checkAddress';
+import path from 'path';
+import { IPayloadsControllerCore_ABI } from '@bgd-labs/aave-address-book';
+import { getTransactionReceipt } from 'viem/actions';
 
 export function addCommand(program: Command) {
   program
@@ -15,16 +19,9 @@ export function addCommand(program: Command) {
     .option('--alias <string>', 'Set a custom alias')
     .option('--proposalId <number>', 'ProposalId to execute')
     .option('--payloadId <number>', 'PayloadId to execute')
-    .option('--payloadsController <string>', 'PayloadsController address')
+    .option('--artifactPath <string>', 'path to the local payload')
     .action(async (options) => {
-      const {
-        chainId,
-        blockNumber,
-        alias,
-        payloadId,
-        proposalId,
-        payloadsController: payloadsControllerAddress,
-      } = options;
+      const { chainId, blockNumber, alias, payloadId, proposalId, artifactPath } = options;
       function getAlias() {
         const unix = Math.floor(new Date().getTime() / 1000);
         if (alias) {
@@ -42,19 +39,57 @@ export function addCommand(program: Command) {
         alias: getAlias(),
         blockNumber: Number(blockNumber),
       };
-      const governance = getGovernance({ address: DEFAULT_GOVERNANCE, client: DEFAULT_GOVERNANCE_CLIENT });
       if (proposalId) {
+        const governance = getGovernance({ address: DEFAULT_GOVERNANCE, client: DEFAULT_GOVERNANCE_CLIENT });
         const payload = await governance.getSimulationPayloadForExecution(BigInt(proposalId));
         const fork = await tenderly.fork({
           ...forkConfig,
           blockNumber: forkConfig.blockNumber || payload.block_number,
         });
         await tenderly.unwrapAndExecuteSimulationPayloadOnFork(fork, payload);
-      } else if (payloadId != undefined) {
-        if (!payloadsControllerAddress) throw new Error('you need to provide a payloadsController');
+        return;
+      }
+      if (payloadId == undefined && artifactPath == undefined) throw new Error('you need to specify an id or artifact');
+      const payloadsControllerAddress = findPayloadsController(forkConfig.chainId);
+      if (!payloadsControllerAddress) throw new Error('payloadscontroller not found on specified chain');
+
+      if (!payloadId) {
+        const fork = await tenderly.fork({
+          ...forkConfig,
+          blockNumber: forkConfig.blockNumber,
+        });
+        const payload = await tenderly.deployCode(fork, path.join(process.cwd(), artifactPath!));
+        const walletProvider = createWalletClient({
+          account: EOA,
+          chain: { id: fork.forkNetworkId, name: 'tenderly' } as any,
+          transport: http(fork.forkUrl),
+        });
+        const payloadsController = getPayloadsController(payloadsControllerAddress as Hex, walletProvider);
+        const hash = await payloadsController.controllerContract.write.createPayload(
+          [
+            [
+              {
+                target: payload,
+                withDelegateCall: true,
+                accessLevel: 1,
+                value: 0n,
+                signature: 'execute()',
+                callData: '0x0',
+              },
+            ],
+          ],
+          {} as any
+        );
+        const tenderlyPayload = await payloadsController.getSimulationPayloadForExecution(
+          Number((await payloadsController.controllerContract.read.getPayloadsCount()) - 1)
+        );
+        await tenderly.unwrapAndExecuteSimulationPayloadOnFork(fork, tenderlyPayload);
+        return;
+      }
+      if (payloadId != undefined) {
         const payloadsController = getPayloadsController(
           payloadsControllerAddress as Hex,
-          CHAIN_ID_CLIENT_MAP[forkConfig.chainId as keyof typeof CHAIN_ID_CLIENT_MAP]
+          CHAIN_ID_CLIENT_MAP[chainId]
         );
         const payload = await payloadsController.getSimulationPayloadForExecution(Number(payloadId));
         const fork = await tenderly.fork({
@@ -62,6 +97,7 @@ export function addCommand(program: Command) {
           blockNumber: forkConfig.blockNumber || payload.block_number,
         });
         await tenderly.unwrapAndExecuteSimulationPayloadOnFork(fork, payload);
+        return;
       }
     });
 }
