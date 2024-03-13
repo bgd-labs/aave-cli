e4ec34f9aa28c76d57eeebbe786b34f723a04313
sakulstra <lukasstrassel@googlemail.com>
2024-03-13 12:05:36 +0100

    fix: improve renderer

--- src/govv3/checks/state.ts
+++ src/govv3/checks/state.ts
@@ -1,11 +1,129 @@
 // Based on https://github.com/Uniswap/governance-seatbelt/blob/main/checks/check-state-changes.ts
 // adjusted for viem & aave governance v3
-import { Address, Hex, getAddress } from 'viem';
+import { Client, Hex, formatUnits, getAddress } from 'viem';
 import { ProposalCheck } from './types';
 import { getContractName } from '../utils/solidityUtils';
-import { StateDiff } from '../../utils/tenderlyClient';
-import { tenderlyDeepDiff } from '../utils/tenderlyDeepDiff';
-import { interpretStateChange } from '../utils/stateDiffInterpreter';
+import { StateDiff, TenderlySimulationResponse } from '../../utils/tenderlyClient';
+import { findAsset } from '../utils/checkAddress';
+import { formatNumberString } from '../utils/markdownUtils';
+
+type ValueType = string | Record<string, string>;
+
+type Change = { name: string; before: string | Record<string, ValueType>; after: string; type?: string };
+
+function getContractChanges(diffs: StateDiff[]) {
+  let changes: Change[] = [];
+  for (const diff of diffs) {
+    if (!diff.soltype) {
+      // In this branch, state change is not decoded, so return raw data of each storage write
+      // (all other branches have decoded state changes)
+      diff.raw.forEach((w) => {
+        const oldVal = JSON.stringify(w.original);
+        const newVal = JSON.stringify(w.dirty);
+        changes.push({ before: oldVal, after: newVal, name: `Slot \`${w.key}\`` });
+      });
+    } else if (diff.soltype.simple_type) {
+      // This is a simple type with a single changed value
+      // const oldVal = JSON.parse(JSON.stringify(diff.original))
+      // const newVal = JSON.parse(JSON.stringify(diff.dirty))
+      changes.push({
+        before: diff.original as string,
+        after: diff.dirty as string,
+        name: diff.soltype.name,
+        type: diff.soltype?.name,
+      });
+    } else if (diff.soltype.type.startsWith('mapping')) {
+      // This is a complex type like a mapping, which may have multiple changes. The diff.original
+      // and diff.dirty fields can be strings or objects, and for complex types they are objects,
+      // so we cast them as such
+      const keys = Object.keys(diff.original);
+      const original = diff.original as Record<string, any>;
+      const dirty = diff.dirty as Record<string, any>;
+      for (const k of keys as Hex[]) {
+        changes.push({ before: original[k], after: dirty[k], name: k, type: diff.soltype?.name });
+      }
+    } else {
+      // TODO arrays and nested mapping are currently not well supported -- find a transaction
+      // that changes state of these types to inspect the Tenderly simulation response and
+      // handle it accordingly. In the meantime we show the raw state changes and print a
+      // warning about decoding the data
+      diff.raw.forEach((w) => {
+        const oldVal = JSON.stringify(w.original);
+        const newVal = JSON.stringify(w.dirty);
+        // info += `\n        - Slot \`${w.key}\` changed from \`${oldVal}\` to \`${newVal}\``
+        changes.push({ before: oldVal, after: newVal, name: `Slot \`${w.key}\`` });
+        console.log(`Could not parse state: add support for formatting type ${diff.soltype?.type} (slot ${w.key})\n`);
+      });
+    }
+  }
+  return changes;
+}
+
+async function renderContractChanges(
+  simulation: TenderlySimulationResponse,
+  client: Client,
+  address: Hex,
+  changes: Change[]
+) {
+  let stateChanges = `\n${getContractName(simulation.contracts, address, client.chain!.id)}\n\`\`\`diff\n`;
+  for (const change of changes) {
+    stateChanges += await deepDiff(client, address, change.before, change.after, change.name, change.type);
+  }
+  stateChanges += '```\n';
+
+  return stateChanges;
+}
+
+function wrapInQuotes(name: string, quotes: boolean) {
+  if (quotes) return '`' + name + '`';
+  return name;
+}
+
+export async function deepDiff(
+  client: Client,
+  address: Hex,
+  before: Record<string, any> | string,
+  after: Record<string, any> | string,
+  name: string,
+  type?: string
+): Promise<string> {
+  if (typeof before !== 'object' || typeof after !== 'object') {
+    return `@@ ${type ? `${wrapInQuotes(type, true)} key ` : ''}${wrapInQuotes(
+      name,
+      !!type
+    )} @@\n- ${await enhanceValue(client, address, before, type)}\n+ ${await enhanceValue(
+      client,
+      address,
+      after,
+      type
+    )}\n`;
+  }
+  let result = '';
+  for (const key of Object.keys(before)) {
+    if (before[key] === after[key]) continue;
+    if (typeof before[key] === 'object')
+      result += await deepDiff(client, address, before[key], after[key], name ? `${name}.${key}` : key, type);
+    else
+      result += `@@ ${type ? `${wrapInQuotes(type, true)} key ` : ''}${wrapInQuotes(
+        `${name}.${key}`,
+        !!type
+      )} @@\n- ${await enhanceValue(client, address, before[key], type)}\n+ ${await enhanceValue(
+        client,
+        address,
+        after[key],
+        type
+      )}\n`;
+  }
+  return result;
+}
+
+async function enhanceValue(client: Client, address: Hex, value: string, type?: string) {
+  if (type && ['_balances', 'balanceOf', 'balances', 'allowed', '_allowances', 'allowance'].includes(type)) {
+    const asset = await findAsset(client, address);
+    if (asset) return `${formatNumberString(formatUnits(BigInt(value), asset.decimals))}[${value}]`;
+  }
+  return value;
+}
 
 export const checkStateChanges: ProposalCheck<any> = {
   name: 'Reports all state changes',
@@ -35,66 +153,8 @@ export const checkStateChanges: ProposalCheck<any> = {
         let warnings = '';
         // Parse state changes at each address
         for (const [address, diffs] of Object.entries(stateDiffs)) {
-          // Use contracts array to get contract name of address
-          stateChanges += `\n${getContractName(
-            simulation.contracts,
-            address as Address,
-            client.chain!.id
-          )}\n\`\`\`diff\n`;
-
-          // Parse each diff. A single diff may involve multiple storage changes, e.g. a proposal that
-          // executes three transactions will show three state changes to the `queuedTransactions`
-          // mapping within a single `diff` element. We always JSON.stringify the values so structs
-          // (i.e. tuples) don't print as [object Object]
-          for (const diff of diffs) {
-            if (!diff.soltype) {
-              // In this branch, state change is not decoded, so return raw data of each storage write
-              // (all other branches have decoded state changes)
-              diff.raw.forEach((w) => {
-                const oldVal = JSON.stringify(w.original);
-                const newVal = JSON.stringify(w.dirty);
-                // info += `\n        - Slot \`${w.key}\` changed from \`${oldVal}\` to \`${newVal}\``
-                stateChanges += tenderlyDeepDiff(oldVal, newVal, `Slot \`${w.key}\``);
-              });
-            } else if (diff.soltype.simple_type) {
-              // This is a simple type with a single changed value
-              // const oldVal = JSON.parse(JSON.stringify(diff.original))
-              // const newVal = JSON.parse(JSON.stringify(diff.dirty))
-              // info += `\n        - \`${diff.soltype.name}\` changed from \`${oldVal}\` to \`${newVal}\``
-              stateChanges += tenderlyDeepDiff(diff.original, diff.dirty, diff.soltype.name);
-            } else if (diff.soltype.type.startsWith('mapping')) {
-              // This is a complex type like a mapping, which may have multiple changes. The diff.original
-              // and diff.dirty fields can be strings or objects, and for complex types they are objects,
-              // so we cast them as such
-              const keys = Object.keys(diff.original);
-              const original = diff.original as Record<string, any>;
-              const dirty = diff.dirty as Record<string, any>;
-              for (const k of keys as Hex[]) {
-                stateChanges += await interpretStateChange(
-                  address as Hex,
-                  diff.soltype?.name,
-                  original[k],
-                  dirty[k],
-                  k,
-                  client
-                );
-                stateChanges += '\n';
-              }
-            } else {
-              // TODO arrays and nested mapping are currently not well supported -- find a transaction
-              // that changes state of these types to inspect the Tenderly simulation response and
-              // handle it accordingly. In the meantime we show the raw state changes and print a
-              // warning about decoding the data
-              diff.raw.forEach((w) => {
-                const oldVal = JSON.stringify(w.original);
-                const newVal = JSON.stringify(w.dirty);
-                // info += `\n        - Slot \`${w.key}\` changed from \`${oldVal}\` to \`${newVal}\``
-                stateChanges += tenderlyDeepDiff(oldVal, newVal, `Slot \`${w.key}\``);
-                warnings += `Could not parse state: add support for formatting type ${diff.soltype?.type} (slot ${w.key})\n`;
-              });
-            }
-          }
-          stateChanges += '```\n';
+          const changes = getContractChanges(diffs);
+          stateChanges += await renderContractChanges(simulation, client, address as Hex, changes);
         }
 
         info.push(stateChanges);
