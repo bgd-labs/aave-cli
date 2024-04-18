197d21e6ac0b388dec2fe5083996a5a096c6c33d
sakulstra <lukasstrassel@googlemail.com>
2024-04-18 14:41:06 +0200

    fix: cleanup mock

--- src/govv3/checks/state.spec.ts
+++ src/govv3/checks/state.spec.ts
@@ -1,11 +1,11 @@
-import { CHAIN_ID_CLIENT_MAP } from "@bgd-labs/js-utils";
-import { describe, expect, it } from "vitest";
-import { CONFIG_CHANGE_PAYLOAD } from "../mocks/configChangePayload";
-import { MOCK_PAYLOAD } from "../mocks/payload";
-import { checkStateChanges } from "./state";
+import {CHAIN_ID_CLIENT_MAP} from '@bgd-labs/js-utils';
+import {describe, expect, it} from 'vitest';
+import {CONFIG_CHANGE_PAYLOAD} from '../mocks/configChangePayload';
+import {MOCK_PAYLOAD} from '../mocks/payload';
+import {checkStateChanges} from './state';
 
-describe("state check", () => {
-  it("should correctly render state diff for", async () => {
+describe('state check', () => {
+  it('should correctly render state diff for', async () => {
     const result = await checkStateChanges.checkProposal(
       null as any,
       MOCK_PAYLOAD.simulation,
@@ -13,8 +13,12 @@ describe("state check", () => {
     );
     expect(result).toMatchSnapshot();
   });
-  it("should correctly render state diff for config change", async () => {
-    const result = await checkStateChanges.checkProposal(null as any, CONFIG_CHANGE_PAYLOAD, CHAIN_ID_CLIENT_MAP[1]);
+  it('should correctly render state diff for config change', async () => {
+    const result = await checkStateChanges.checkProposal(
+      null as any,
+      CONFIG_CHANGE_PAYLOAD,
+      CHAIN_ID_CLIENT_MAP[1],
+    );
     expect(result).toMatchSnapshot();
   });
 });
--- src/govv3/generatePayloadReport.spec.ts
+++ src/govv3/generatePayloadReport.spec.ts
@@ -1,14 +1,34 @@
-import { CHAIN_ID_CLIENT_MAP } from "@bgd-labs/js-utils";
-import { describe, expect, it } from "vitest";
-import { generateReport } from "./generatePayloadReport";
-import { MOCK_PAYLOAD } from "./mocks/payload";
-import { STREAM_PAYLOAD } from "./mocks/streamPayload";
-import { findPayloadsController } from "./utils/checkAddress";
-import { getPayloadsController } from "./payloadsController";
-import { cachePayloadsController } from "./cache/updateCache";
-import { Address } from "viem";
+import {CHAIN_ID_CLIENT_MAP} from '@bgd-labs/js-utils';
+import {describe, expect, it} from 'vitest';
+import {generateReport} from './generatePayloadReport';
+import {MOCK_PAYLOAD} from './mocks/payload';
+import {STREAM_PAYLOAD} from './mocks/streamPayload';
+import {findPayloadsController} from './utils/checkAddress';
+import {getPayloadsController} from './payloadsController';
+import {Address} from 'viem';
+import {localCacheAdapter} from '@bgd-labs/aave-v3-governance-cache/localCache';
 
-describe("generatePayloadReport", () => {
+/**
+ * Tenderly simulation results are insanely huge, so we're removing some stuff we don't need
+ * @param mockData
+ */
+function cleanupMock(mockData: any) {
+  mockData.simulation.contracts.map((ctr: any, ix: number) => {
+    delete mockData.simulation.contracts[ix].src_map;
+    delete mockData.simulation.contracts[ix].deployed_bytecode;
+    delete mockData.simulation.contracts[ix].creation_bytecode;
+    delete mockData.simulation.contracts[ix].data;
+  });
+
+  return JSON.stringify(mockData, (key, value) =>
+    typeof value === 'bigint' ? value.toString() : value,
+  );
+}
+
+describe('generatePayloadReport', () => {
+  it.skip('cleanup', () => {
+    expect(cleanupMock(MOCK_PAYLOAD)).toMatchSnapshot();
+  });
   /**
    * Can be used to generate a new snapshot
    */
@@ -18,15 +38,20 @@ describe("generatePayloadReport", () => {
     const client = CHAIN_ID_CLIENT_MAP[chainId];
     const payloadsControllerAddress = findPayloadsController(Number(chainId));
     const payloadsController = getPayloadsController(payloadsControllerAddress as Address, client);
-    const { eventsCache } = await cachePayloadsController(client, payloadsControllerAddress as Address);
-    const config = await payloadsController.getPayload(payloadId, eventsCache);
-    const result = await payloadsController.simulatePayloadExecutionOnTenderly(Number(payloadId), config);
-    result.contracts.map((ctr, ix) => delete result.contracts[ix].src_map);
-    console.log(JSON.stringify({simulation: result, payloadInfo: config}, (key, value) => (typeof value === "bigint" ? value.toString() : value), 2));
+    const cache = await localCacheAdapter.getPayload({
+      chainId,
+      payloadId,
+      payloadsController: payloadsControllerAddress!,
+    });
+    const result = await payloadsController.simulatePayloadExecutionOnTenderly(
+      Number(payloadId),
+      cache.logs,
+    );
+    cleanupMock({simulation: result, payloadInfo: cache});
   });
 
   it(
-    "should match snapshot listing",
+    'should match snapshot listing',
     async () => {
       const report = await generateReport({
         ...(MOCK_PAYLOAD as any),
@@ -34,11 +59,11 @@ describe("generatePayloadReport", () => {
       });
       expect(report).toMatchSnapshot();
     },
-    { timeout: 30000 },
+    {timeout: 30000},
   );
 
   it(
-    "should match snapshot streams",
+    'should match snapshot streams',
     async () => {
       const report = await generateReport({
         ...(STREAM_PAYLOAD as any),
@@ -46,6 +71,6 @@ describe("generatePayloadReport", () => {
       });
       expect(report).toMatchSnapshot();
     },
-    { timeout: 30000 },
+    {timeout: 30000},
   );
 });
