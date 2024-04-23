4549bea7465de392e4226f5d4aca6fdff5dc3710
sakulstra <lukasstrassel@googlemail.com>
2024-04-23 16:03:02 +0200

    fix: patch log interpreter

--- src/govv3/utils/logInterpreter.ts
+++ src/govv3/utils/logInterpreter.ts
@@ -15,20 +15,35 @@ export async function interpretLog(
   client: Client,
   address: Hex,
   name: string | null,
-  inputs: Input[],
+  inputs: Input[] | null,
 ) {
-  for (let i = 0; i < inputs.length; i++) {
-    if (inputs[i].soltype?.name && assetFields.includes(inputs[i].soltype!.name)) {
-      inputs[i].value = await addAssetSymbol(client, inputs[i].value as Address);
+  if (inputs) {
+    for (let i = 0; i < inputs.length; i++) {
+      if (inputs[i].soltype?.name && assetFields.includes(inputs[i].soltype!.name)) {
+        inputs[i].value = await addAssetSymbol(client, inputs[i].value as Address);
+      }
     }
-  }
-  if (name && tokenAmountEvents.includes(name)) {
-    // fields formatted by the asset decimal
-    const decimalFieldNames = ['value', 'amount', 'wad'];
-    for (const name of decimalFieldNames) {
-      const valueIndex = inputs.findIndex((i) => i.soltype!.name === name);
-      if (valueIndex !== -1) {
-        const asset = await findAsset(client, address);
+    if (name && tokenAmountEvents.includes(name)) {
+      // fields formatted by the asset decimal
+      const decimalFieldNames = ['value', 'amount', 'wad'];
+      for (const name of decimalFieldNames) {
+        const valueIndex = inputs.findIndex((i) => i.soltype!.name === name);
+        if (valueIndex !== -1) {
+          const asset = await findAsset(client, address);
+          if (asset) {
+            inputs[valueIndex].value = prettifyNumber({
+              value: inputs[valueIndex].value as string,
+              decimals: asset.decimals,
+            });
+          }
+        }
+      }
+    }
+    if (name && reserveEvents.includes(name)) {
+      const valueIndex = inputs.findIndex((i) => i.soltype!.name === 'amount');
+      const reserveIndex = inputs.findIndex((i) => i.soltype!.name === 'reserve');
+      if (valueIndex !== -1 && reserveIndex !== -1) {
+        const asset = await findAsset(client, inputs[reserveIndex].value as Hex);
         if (asset) {
           inputs[valueIndex].value = prettifyNumber({
             value: inputs[valueIndex].value as string,
@@ -37,30 +52,17 @@ export async function interpretLog(
         }
       }
     }
-  }
-  if (name && reserveEvents.includes(name)) {
-    const valueIndex = inputs.findIndex((i) => i.soltype!.name === 'amount');
-    const reserveIndex = inputs.findIndex((i) => i.soltype!.name === 'reserve');
-    if (valueIndex !== -1 && reserveIndex !== -1) {
-      const asset = await findAsset(client, inputs[reserveIndex].value as Hex);
-      if (asset) {
-        inputs[valueIndex].value = prettifyNumber({
-          value: inputs[valueIndex].value as string,
-          decimals: asset.decimals,
-        });
-      }
-    }
-  }
-  if (name && ['Mint', 'ReserveDataUpdated', 'Burn'].includes(name)) {
-    // fields formatted by the asset decimal
-    const decimalFieldNames = ['liquidityIndex', 'variableBorrowIndex', 'index'];
-    for (const name of decimalFieldNames) {
-      const valueIndex = inputs.findIndex((i) => i.soltype!.name === name);
-      if (valueIndex !== -1) {
-        inputs[valueIndex].value = prettifyNumber({
-          value: inputs[valueIndex].value as string,
-          decimals: 27,
-        });
+    if (name && ['Mint', 'ReserveDataUpdated', 'Burn'].includes(name)) {
+      // fields formatted by the asset decimal
+      const decimalFieldNames = ['liquidityIndex', 'variableBorrowIndex', 'index'];
+      for (const name of decimalFieldNames) {
+        const valueIndex = inputs.findIndex((i) => i.soltype!.name === name);
+        if (valueIndex !== -1) {
+          inputs[valueIndex].value = prettifyNumber({
+            value: inputs[valueIndex].value as string,
+            decimals: 27,
+          });
+        }
       }
     }
   }
