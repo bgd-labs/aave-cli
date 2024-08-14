937a3ec64796e84f886f2a0f9be1674a63f5519a
sakulstra <lukasstrassel@googlemail.com>
2024-08-14 16:30:48 +0200

    fix: patch utils

--- src/reports/diff-reports.ts
+++ src/reports/diff-reports.ts
@@ -6,6 +6,15 @@ import {renderReserve, renderReserveDiff} from './reserve';
 import {AaveV3Reserve, type AaveV3Snapshot} from './snapshot-types';
 import {renderStrategy, renderStrategyDiff} from './strategy';
 
+function hasDiff(input: Record<string, any>): boolean {
+  return !!Object.keys(input).map(
+    (key) =>
+      typeof input[key as keyof typeof input] === 'object' &&
+      (input[key as keyof typeof input].hasOwnProperty('from') ||
+        input[key as keyof typeof input].hasOwnProperty('to')),
+  );
+}
+
 export async function diffReports<A extends AaveV3Snapshot, B extends AaveV3Snapshot>(
   pre: A,
   post: B,
@@ -55,16 +64,15 @@ export async function diffReports<A extends AaveV3Snapshot, B extends AaveV3Snap
     .map((reserveKey) => {
       // "from" being present on reserses key means reserve was removed
       if (!(diffResult.reserves[reserveKey] as any).hasOwnProperty('from')) {
-        const hasChangedReserveProperties = Object.keys(diffResult.reserves[reserveKey]).find(
-          (fieldKey) => typeof (diffResult.reserves as any)[reserveKey][fieldKey] === 'object',
-        );
+        const hasChangedReserveProperties = hasDiff(diffResult.reserves[reserveKey]);
         const preIrHash = hash(pre.strategies[reserveKey]);
         const postIrHash = hash(post.strategies[reserveKey]);
         const hasChangedIr = preIrHash !== postIrHash;
         const eModeCategoryChanged =
           diffResult.reserves[reserveKey].eModeCategory?.hasOwnProperty('from');
-        const eModeParamsChanged =
-          diffResult.eModes?.[diffResult.reserves[reserveKey].eModeCategory as any];
+        const eModeParamsChanged = hasDiff(
+          diffResult.eModes?.[diffResult.reserves[reserveKey].eModeCategory as any] || {},
+        );
         if (
           !hasChangedReserveProperties &&
           !hasChangedIr &&
@@ -91,7 +99,7 @@ export async function diffReports<A extends AaveV3Snapshot, B extends AaveV3Snap
           );
         }
 
-        if (eModeParamsChanged) {
+        if (eModeParamsChanged && !eModeCategoryChanged) {
           report += renderEmodeDiff(
             diff(
               pre.eModes[diffResult.reserves[reserveKey].eModeCategory as any] || {},
