97947bf72ec4775fc3c966afd20a60db1bab7b47
sakulstra <lukasstrassel@googlemail.com>
2024-08-14 16:22:53 +0200

    fix: always print ir changes

--- src/reports/diff-reports.ts
+++ src/reports/diff-reports.ts
@@ -53,30 +53,49 @@ export async function diffReports<A extends AaveV3Snapshot, B extends AaveV3Snap
     .filter((i) => i);
   const reservesAltered = Object.keys(diffResult.reserves)
     .map((reserveKey) => {
-      // from being present on key means reserve was removed
-      if (
-        !(diffResult.reserves[reserveKey] as any).hasOwnProperty('from') &&
-        Object.keys(diffResult.reserves[reserveKey]).find(
+      // "from" being present on reserses key means reserve was removed
+      if (!(diffResult.reserves[reserveKey] as any).hasOwnProperty('from')) {
+        const hasChangedReserveProperties = Object.keys(diffResult.reserves[reserveKey]).find(
           (fieldKey) => typeof (diffResult.reserves as any)[reserveKey][fieldKey] === 'object',
+        );
+        const preIrHash = hash(pre.strategies[reserveKey]);
+        const postIrHash = hash(post.strategies[reserveKey]);
+        const hasChangedIr = preIrHash !== postIrHash;
+        const eModeCategoryChanged =
+          diffResult.reserves[reserveKey].eModeCategory?.hasOwnProperty('from');
+        const eModeParamsChanged =
+          diffResult.eModes?.[diffResult.reserves[reserveKey].eModeCategory as any];
+        if (
+          !hasChangedReserveProperties &&
+          !hasChangedIr &&
+          !eModeCategoryChanged &&
+          !eModeParamsChanged
         )
-      ) {
+          return;
         // diff reserve
         let report = renderReserveDiff(diffResult.reserves[reserveKey] as any, chainId);
         // diff irs
-        const preIrHash = hash(pre.strategies[reserveKey]);
-        const postIrHash = hash(post.strategies[reserveKey]);
-        if (preIrHash !== postIrHash) {
+        if (hasChangedIr) {
           report += renderStrategyDiff(
             diff(pre.strategies[reserveKey], post.strategies[reserveKey]) as any,
           );
           report += `| interestRate | ![before](/.assets/${preIrHash}.svg) | ![after](/.assets/${postIrHash}.svg) |`;
         }
         // diff eModes
-        if (diffResult.reserves[reserveKey].eModeCategory?.hasOwnProperty('from')) {
+        if (eModeCategoryChanged) {
           report += renderEmodeDiff(
             diff(
               pre.eModes[(diffResult.reserves[reserveKey].eModeCategory as any).from] || {},
               post.eModes[(diffResult.reserves[reserveKey].eModeCategory as any).to],
+            ) as any,
+          );
+        }
+
+        if (eModeParamsChanged) {
+          report += renderEmodeDiff(
+            diff(
+              pre.eModes[diffResult.reserves[reserveKey].eModeCategory as any] || {},
+              post.eModes[diffResult.reserves[reserveKey].eModeCategory as any],
             ) as any,
           );
         }
