95bb48699fca160f247a5afdc0a04ec8669ebfad
Lukas <ls@bgdlabs.com>
2025-06-02 11:37:01 +0200

    fix: update etherscan api key usage (#212)
    foundry upgraded to etherscan v2, so no api keys are needed for each chain.

--- src/reports/code-diff.ts
+++ src/reports/code-diff.ts
@@ -1,28 +1,10 @@
 import {existsSync} from 'fs';
 import {execSync} from 'child_process';
-import {ChainId} from '@bgd-labs/rpc-env';
-
-const CHAIN_ID_TO_ETHERSCAN = {
-  [ChainId.mainnet]: process.env.ETHERSCAN_API_KEY_MAINNET,
-  [ChainId.polygon]: process.env.ETHERSCAN_API_KEY_POLYGON,
-  [ChainId.bnb]: process.env.ETHERSCAN_API_KEY_BNB,
-  [ChainId.base]: process.env.ETHERSCAN_API_KEY_BASE,
-  [ChainId.arbitrum]: process.env.ETHERSCAN_API_KEY_ARBITRUM,
-  [ChainId.optimism]: process.env.ETHERSCAN_API_KEY_OPTIMISM,
-  [ChainId.gnosis]: process.env.ETHERSCAN_API_KEY_GNOSIS,
-  [ChainId.avalanche]: process.env.ETHERSCAN_API_KEY_AVALANCHE,
-  [ChainId.zksync]: process.env.ETHERSCAN_API_KEY_ZKSYNC,
-  [ChainId.scroll]: process.env.ETHERSCAN_API_KEY_SCROLL,
-  [ChainId.linea]: process.env.ETHERSCAN_API_KEY_LINEA,
-  [ChainId.celo]: process.env.ETHERSCAN_API_KEY_CELO,
-  [ChainId.mantle]: process.env.ETHERSCAN_API_KEY_MANTLE,
-  [ChainId.sonic]: process.env.ETHERSCAN_API_KEY_SONIC,
-};
 
 export function downloadContract(chainId: number, address: string) {
   const outPath = `/tmp/${chainId}_${address}`;
   if (existsSync(outPath)) console.log('skipped download');
-  const command = `cast source --chain ${chainId} -d ${outPath} ${address} --etherscan-api-key ${CHAIN_ID_TO_ETHERSCAN[chainId as keyof typeof CHAIN_ID_TO_ETHERSCAN]} && forge fmt ${outPath}`;
+  const command = `cast source --chain ${chainId} -d ${outPath} ${address} --etherscan-api-key ${process.env.ETHERSCAN_API_KEY} && forge fmt ${outPath}`;
   execSync(command);
   return outPath;
 }
