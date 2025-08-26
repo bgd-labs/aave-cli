c1f31ec31773aecf792590388a0c1314811f8029
Lukas <lukasstrassel@googlemail.com>
2025-08-26 12:48:20 +0200

    fix: remove obsolete method

--- src/commands/diffSnaphots.ts
+++ src/commands/diffSnaphots.ts
@@ -1,6 +1,5 @@
 import fs from 'node:fs';
 import path from 'node:path';
-import {compareStorageLayouts} from '@bgd-labs/js-utils';
 import type {Command} from '@commander-js/extra-typings';
 import {adiDiffReports} from '../reports/adi-diff-reports';
 import {readJsonFile, readJsonString} from '../utils/json';
@@ -17,24 +16,6 @@ export function addCommand(program: Command) {
       const from = options.stringMode ? readJsonString(_from) : readJsonFile(_from);
       const to = options.stringMode ? readJsonString(_to) : readJsonFile(_to);
       const content = await adiDiffReports(from, to);
-      if (options.out) {
-        fs.mkdirSync(path.dirname(options.out), {recursive: true});
-        fs.writeFileSync(options.out, content);
-      } else {
-        console.log(content);
-      }
-    });
-
-  program
-    .command('diff-storage')
-    .description('generate a storage diff')
-    .argument('<from>')
-    .argument('<to>')
-    .option('-o, --out <string>', 'output path')
-    .action(async (_from, _to, options) => {
-      const from = readJsonFile(_from);
-      const to = readJsonFile(_to);
-      const content = await compareStorageLayouts(from, to);
       if (options.out) {
         fs.mkdirSync(path.dirname(options.out), {recursive: true});
         fs.writeFileSync(options.out, content);
