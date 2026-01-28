c9202ecaa52ed6e405bdd8832b7501cbf6999c4a
Harsh Pandey <harshsatishpandey@gmail.com>
2026-01-28 14:10:18 +0530

    fix: new chains / bump packages (#222)
    * fix: bump package
    
    * fix: test and npm warnings
    
    * chore: update snpashot for test to pass

--- src/reports/adi-diff-report.spec.ts
+++ src/reports/adi-diff-report.spec.ts
@@ -3,14 +3,10 @@ import {readJsonFile} from '../utils/json';
 import {adiDiffReports} from './adi-diff-reports';
 
 describe('adi report', () => {
-  it(
-    'should generate a well formatted adi report',
-    async () => {
-      const from = readJsonFile('/src/reports/mocks/preTestADI.json');
-      const to = readJsonFile('/src/reports/mocks/postTestADI.json');
-      const content = await adiDiffReports(from, to);
-      expect(content).toMatchSnapshot();
-    },
-    {timeout: 30000},
-  );
+  it('should generate a well formatted adi report', {timeout: 30000}, async () => {
+    const from = readJsonFile('/src/reports/mocks/preTestADI.json');
+    const to = readJsonFile('/src/reports/mocks/postTestADI.json');
+    const content = await adiDiffReports(from, to);
+    expect(content).toMatchSnapshot();
+  });
 });
